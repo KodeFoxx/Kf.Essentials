a207aa0c641c2e26143fd2c33d328cdbe328fd38
Yves Schelpe <yves.schelpe@kdg.be>
2020-02-24 15:44:38 +0100

    IDebuggerDisplayString

---
+++ Source/Kf.Essentials/Diagnostics/IDebuggerDisplayString.cs
@@ -0,0 +1,25 @@
+﻿namespace Kf.Essentials.Diagnostics
+{
+    /// <summary>
+    /// Interface to facilitate implementing the <see cref="DebuggerDisplayAttribute"/>.
+    /// </summary>
+    /// <example>
+    /// Place the attribute on your class as shown below.
+    /// <code>
+    /// [DebuggerDisplay("{DebuggerDisplayString,nq}")]
+    /// </code>
+    /// </example>
+    public interface IDebuggerDisplayString
+    {
+        /// <summary>
+        /// The string intended to be used in the <see cref="DebuggerDisplayAttribute"/>.
+        /// </summary>
+        /// <example>
+        /// Place the attribute on your class as shown below.
+        /// <code>
+        /// [DebuggerDisplay("{DebuggerDisplayString,nq}")]
+        /// </code>
+        /// </example>
+        string DebuggerDisplayString { get; }
+    }
+}
