c63222f7a1aa1662c7789c7e674a086095fd582b
Yves Schelpe <yves.schelpe@kdg.be>
2020-02-24 21:51:41 +0100

    GetPropertyInfo test

--- Tests/Kf.Essentials.Tests.UnitTests/System/ObjectExtenions.ReflectionTests.cs
+++ Tests/Kf.Essentials.Tests.UnitTests/System/ObjectExtenions.ReflectionTests.cs
@@ -1,12 +1,26 @@
 ﻿using FluentAssertions;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using Xunit;
 
 namespace Kf.Essentials.Tests.UnitTests.System
 {
     public sealed class ObjectExtenions
     {
+        [Fact]
+        public void GetPropertyInfo_returns_the_selected_PropertyInfo()
+        {
+            var stringObject = "stringObject";
+
+            var sut = stringObject.GetPropertyInfo(p => p.Length)
+                .Some(v => v)
+                .None(() => null);
+
+            sut.Should().NotBeNull();
+            sut.Name.Should().Be(nameof(stringObject.Length));            
+        }
+
         [Fact]
         public void GetPropertyNameAndValue_returns_the_name_and_value_of_the_property()
         {
