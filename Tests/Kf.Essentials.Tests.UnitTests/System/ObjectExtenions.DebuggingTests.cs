43bebb0016b614f1e9410db6f59a8462083ce342
Yves Schelpe <yves.schelpe@kdg.be>
2020-02-25 13:11:36 +0100

    Moved debugging tests to seperate file

---
+++ Tests/Kf.Essentials.Tests.UnitTests/System/ObjectExtenions.DebuggingTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Kf.Essentials.Tests.UnitTests.System
+{
+    public sealed class ObjectExtenions_DebuggingTests
+    {
+        [Theory, MemberData(nameof(CreateDebugString_with_expressions_creates_correct_string_TestData))]
+        public void CreateDebugString_with_expressions_creates_correct_string(
+            string actual, string expected)
+            => actual
+                .Should().Be(expected);
+
+        public static IEnumerable<object[]> CreateDebugString_with_expressions_creates_correct_string_TestData()
+            => new List<object[]>
+            {
+                new object[] {
+                    KeyValuePair.Create("key", "value")
+                        .CreateDebugString(kvp => kvp.Key, kvp => kvp.Value),
+                    "KeyValuePair<String, String> -> [ Key='key', Value='value' ]"
+                },
+                new object[] {
+                    KeyValuePair.Create("key", (string)null)
+                        .CreateDebugString(kvp => kvp.Key, kvp => kvp.Value),
+                    $"KeyValuePair<String, String> -> [ Key='key', Value='' ]"
+                },
+                new object[] {
+                    KeyValuePair.Create("key", KeyValuePair.Create("number", 1))
+                        .CreateDebugString(kvp => kvp.Key),
+                    $"KeyValuePair<String, KeyValuePair<String, Int32>> -> [ Key='key' ]"
+                },
+                new object[] {
+                    new Exception("message", null)
+                        .CreateDebugString(ex => ex.Message, ex => ex.InnerException),
+                    $"Exception -> [ Message='message', InnerException='' ]"
+                }
+            };
+    }
+}
--- Tests/Kf.Essentials.Tests.UnitTests/System/ObjectExtenions.ReflectionTests.cs
+++ Tests/Kf.Essentials.Tests.UnitTests/System/ObjectExtenions.ReflectionTests.cs
@@ -1,12 +1,10 @@
 ﻿using FluentAssertions;
 using System;
-using System.Collections.Generic;
-using System.Reflection;
 using Xunit;
 
 namespace Kf.Essentials.Tests.UnitTests.System
 {
-    public sealed class ObjectExtenions
+    public sealed class ObjectExtenions_ReflectionTests
     {
         [Fact]
         public void GetPropertyInfo_returns_the_selected_PropertyInfo()
@@ -68,37 +66,6 @@ namespace Kf.Essentials.Tests.UnitTests.System
 
             sut.IsNullOrWhiteSpace().Should().BeTrue();
             sut.Should().Be(String.Empty);
-        }
-
-        [Theory, MemberData(nameof(CreateDebugString_with_expressions_creates_correct_string_TestData))]
-        public void CreateDebugString_with_expressions_creates_correct_string(
-            string actual, string expected)
-            => actual
-                .Should().Be(expected);
-
-        public static IEnumerable<object[]> CreateDebugString_with_expressions_creates_correct_string_TestData()
-            => new List<object[]>
-            {
-                new object[] {
-                    KeyValuePair.Create("key", "value")
-                        .CreateDebugString(kvp => kvp.Key, kvp => kvp.Value),
-                    "KeyValuePair<String, String> -> [ Key='key', Value='value' ]"
-                },
-                new object[] {
-                    KeyValuePair.Create("key", (string)null)
-                        .CreateDebugString(kvp => kvp.Key, kvp => kvp.Value),
-                    $"KeyValuePair<String, String> -> [ Key='key', Value='' ]"
-                },
-                new object[] {
-                    KeyValuePair.Create("key", KeyValuePair.Create("number", 1))
-                        .CreateDebugString(kvp => kvp.Key),
-                    $"KeyValuePair<String, KeyValuePair<String, Int32>> -> [ Key='key' ]"
-                },
-                new object[] {
-                    new Exception("message", null)
-                        .CreateDebugString(ex => ex.Message, ex => ex.InnerException),
-                    $"Exception -> [ Message='message', InnerException='' ]"
-                }
-            };
+        }        
     }
 }
